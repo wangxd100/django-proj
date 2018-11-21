a7970ad9b4013c36ca2f285c248a5ed5a80a7e25
WANGXIAO <wangxd100@gmail.com>
2018-11-21 11:28:13 -0500

    django install web site test app test

---
+++ sample.py
@@ -0,0 +1,17 @@
+import django
+
+print(django.get_version())
+
+print('once installed Django')
+print("--------- how to create a web site ---------")
+print('go to Terminal :: ')
+print('create a website >django-amdin startproject mysite')
+print("run server go to cd mysite:  mysite>python manage.py runserver")
+print("--------- how to create a app ---------")
+print("start a new terminal")
+print("go to my site: cd mysite")
+print(">python manage.py startapp myapp")
+print("app will be creted shortly!")
+
+
+
